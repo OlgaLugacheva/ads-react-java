fa05ed1e8b4c48e0959fd2039ee3cf0cf743a674
Sasha Stepanova <chernishevskie@yandex.ru>
2022-06-18 17:14:56 +0300

    feat: added protectedRoute component

---
+++ src/components/protectedRoute/ProtectedRoute.js
@@ -0,0 +1,9 @@
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ user, redirectPath = "/landing", children }) => {
+  if (!user) {
+    return <Navigate to={redirectPath} replace />;
+  }
+
+  return children;
+};
