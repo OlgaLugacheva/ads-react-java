bac12c7fa5198d2b90b7e42b5f7cbd7f9890cec0
Sasha Stepanova <chernishevskie@yandex.ru>
2022-06-26 23:52:38 +0300

    refactor: changed redirect location

--- src/components/protectedRoute/ProtectedRoute.js
+++ src/components/protectedRoute/ProtectedRoute.js
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ user, redirectPath = "/landing", children }) => {
+const ProtectedRoute = ({ user, redirectPath = "/", children }) => {
   if (!user) {
     return <Navigate to={redirectPath} replace />;
   }
