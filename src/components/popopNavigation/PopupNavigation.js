c6f1ec9610f29f480695810d0f2c945ce71ff22e
Sasha Stepanova <chernishevskie@yandex.ru>
2022-06-29 23:30:58 +0300

    refactor

--- src/components/popopNavigation/PopupNavigation.js
+++ src/components/popopNavigation/PopupNavigation.js
@@ -14,8 +14,13 @@ function PopupNavigation(props) {
           onClick={props.onClose}
           className="popupNavigation__close-button close-button"
         ></button>
-        <Navigation onClose={props.onClose}/>
-        <Button onClose={props.onClose} logOut={props.logOut} className="button button__text" text="Выйти"/>
+        <Navigation onClose={props.onClose} />
+        <Button
+          onClose={props.onClose}
+          logOut={props.logOut}
+          className="button button__text"
+          text="Выйти"
+        />
       </div>
     </div>
   );
