434ce2f6f9b6e2099c9a299383487853a0f3d526
Sasha Stepanova <chernishevskie@yandex.ru>
2022-07-12 23:19:38 +0300

    refactor: changed requests and headers for apis

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,62 +6,93 @@ class Api {
 
   _handleResponse(res) {
     if (res.ok) {
-      console.log(JSON.parse("res"));
       return res.json();
     }
     return Promise.reject(`Error: ${res.status}`);
   }
   //user
-  getUserInfo() {
-    return fetch(`${this._url}/users/me/`, {
+  getUserInfo = async () => {
+    return await fetch(`${this._url}/users/me`, {
       method: "GET",
       headers: this._headers,
     }).then(this._handleResponse);
-  }
+  };
 
-  getUsersAds(page) {
-    return fetch(`${this._url}/ads/me/?page=${page}`, {
+  getUsersAds = async () => {
+    return await fetch(`${this._url}/ads/me`, {
       method: "GET",
       headers: this._headers,
     }).then(this._handleResponse);
-  }
+  };
 
-  updateUser(data) {
-    return fetch(`${this._url}/users/me/`, {
+  updateUser(data, username, password) {
+    return fetch(`${this._url}/users/me`, {
       method: "PATCH",
+      body: JSON.stringify(data),
       headers: {
         "Content-Type": "application/json",
-        Authorization: this._auth,
+        Authorization: `Basic ${username} ${password}`,
       },
-      body: JSON.stringify(data),
     }).then(this._handleResponse);
   }
 
   updateUserPhoto(image) {
     const formData = new FormData();
     formData.append("image", image);
-    return fetch(`${this._url}/users/me/`, formData, {
+    return fetch(`${this._url}/users/me`, formData, {
       method: "PATCH",
       headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: this._auth,
+        "Content-Type": "application/json",
+        Authorization: "Basic user password",
       },
     }).then(this._handleResponse);
   }
 
   //comment|comments
   getComments(ad_pk) {
-    return fetch(`${this._url}/ads/${ad_pk}/comments/`, {
+    return fetch(`${this._url}/ads/${ad_pk}/comments`, {
       method: "GET",
       headers: this._headers,
     }).then(this._handleResponse);
   }
 
   getComment(adId, commentId) {
-    return fetch(`${this._url}/ads/${adId}/comments/${commentId}/`, {
+    return fetch(`${this._url}/ads/${adId}/comments/${commentId}`, {
       method: "GET",
       headers: this._headers,
     }).then(this._handleResponse);
+  }
+
+  addComment(id, text) {
+    return fetch(`${this._url}/ads/${id}/comments`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic user password",
+      },
+      body: JSON.stringify(text),
+    }).then(this._handleResponse);
+  }
+
+  editComment(adId, commentId, data) {
+    return fetch(`${this._url}/ads/${adId}/comments/${commentId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic user password",
+      },
+      body: JSON.stringify(data),
+    }).then(this._handleResponse);
+  }
+
+  deleteComment(adId, commentId) {
+    return fetch(`${this._url}/ads/${adId}/comments/${commentId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic user password",
+      },
+    });
   }
 
   //ads
@@ -86,22 +117,30 @@ class Api {
     formData.append("price", `${price}`);
     formData.append("description", `${description}`);
 
-    fetch(`${this._url}/ads/`, formData, {
+    fetch(`${this._url}/ads`, formData, {
       method: "POST",
       headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: this._auth,
+        "Content-Type": "application/json",
+        Authorization: "Basic user password",
       },
+    }).then(this._handleResponse);
+  }
+
+  //get Ad
+  getAd(id) {
+    return fetch(`${this._url}/ads/${id}`, {
+      method: "GET",
+      headers: this._headers,
     }).then(this._handleResponse);
   }
 
   //edit ad
   editAdd(id, data) {
-    return fetch(`${this._url}/ads/${id}/`, {
+    return fetch(`${this._url}/ads/${id}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
-        Authorization: this._auth,
+        Authorization: "Basic user password",
       },
       body: JSON.stringify(data),
     }).then(this._handleResponse);
@@ -110,35 +149,24 @@ class Api {
   editAddPhoto(id, image) {
     const formData = new FormData();
     formData.append("image", image);
-    return fetch(`${this._url}/ads/${id}/`, formData, {
+    return fetch(`${this._url}/ads/${id}`, formData, {
       method: "PATCH",
       headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: this._auth,
+        "Content-Type": "application/json",
+        Authorization: "Basic user password",
       },
     }).then(this._handleResponse);
   }
 
   //delite add
   deleteAdd(id) {
-    return fetch(`${this._url}/ads/${id}/`, {
+    return fetch(`${this._url}/ads/${id}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
-        Authorization: this._auth,
+        Authorization: "Basic user password",
       },
     });
-  }
-
-  addComment(id, text) {
-    return fetch(`${this._url}/ads/${id}/comments/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: this._auth,
-      },
-      body: JSON.stringify(text),
-    }).then(this._handleResponse);
   }
 }
 
